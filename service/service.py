4ba7f6dc33ed590b4863b7f253f7fef469df201a
morrisalp <morrisalp@gmail.com>
2020-04-05 11:54:41 +0300

    add placeholder /search API call

--- service/service.py
+++ service/service.py
@@ -1,14 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import json
+
+with open('service/sample.json', 'r') as f:
+    sample_response = json.load(f)
 
 app = Flask(__name__)
 
 print("Starting API...")
 
-@app.route("/", methods = ["POST"])
+@app.route("/search", methods = ["POST"])
 def root():
     print("Processing POST request")
     request_json = request.get_json()
     print("Request JSON:", str(request_json))
+    return jsonify(sample_response)
 
 
 @app.route("/test")
